a788ad83923f9143db4a628e8b874f84171b07ad
olesa1212 <o6635970@gmail.com>
2026-05-07 14:28:11 +0300

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,71 @@
+void main() {
+    Student[] arr = new Student[3];
+    Scanner scan1 = new Scanner(System.in);
+    Scanner scan2 = new Scanner(System.in);
+    for(int i = 0; i < arr.length; i++){
+        System.out.println("Введите ФИО:");
+        String fio = scan1.nextLine();
+        System.out.println("Введите год поступления:");
+        int date = scan2.nextInt();
+        System.out.println("Введите возраст:");
+        int age = scan2.nextInt();
+        System.out.println("Введите телефон:");
+        String tel = scan1.nextLine();
+        System.out.println("Введите курс:");
+        String kurs = scan1.nextLine();
+        System.out.println("Введите факультет:");
+        String fac = scan1.nextLine();
+        arr[i] = new Student(age, fio, tel, kurs, fac, date);
+    }
+    System.out.println();
+    for(int i = 0; i < arr.length; i++){
+        System.out.println(arr[i].fio + " " + arr[i].getAge());
+    }
+    System.out.println("\nВведите общий факультет:");
+    String a = scan1.nextLine();
+    for(int i = 0; i < arr.length; i++){
+        if(a.equals(arr[i].fac)) {
+            System.out.println(arr[i].fio);
+        }
+    }
+    System.out.println("\nВведите минимальный год:");
+    int b = scan2.nextInt();
+    for(int i = 0; i < arr.length; i++){
+        if(b <= arr[i].getDate()) {
+            System.out.println(arr[i].fio);
+        }
+    }
+}
+class Person{
+    private int age;
+    public String fio;
+    public String tel;
+    Person(int age, String fio, String tel){
+        this.age = age;
+        this.fio = fio;
+        this.tel = tel;
+    }
+    void setAge(int age){
+        this.age = age;
+    }
+    int getAge(){
+        return this.age;
+    }
+}
+class Student extends Person{
+    public String kurs;
+    public String fac;
+    private int date;
+    Student(int age, String fio, String tel, String kurs, String fac, int date){
+        super(age, fio, tel);
+        this.kurs = kurs;
+        this.fac = fac;
+        this.date = date;
+    }
+    void setDate(int date){
+        this.date = date;
+    }
+    int getDate(){
+        return this.date;
+    }
+}
